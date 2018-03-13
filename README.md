ef8d2538072c9112782e0f6811fe142b4ecd8d38
Chris Dostert <chris.dostert@live.com>
2018-03-12 18:44:37 -0700

    chore) rename

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-[![Build Status](https://travis-ci.org/opspec-pkgs/opspec.op.refs.list.svg?branch=master)](https://travis-ci.org/opspec-pkgs/opspec.op.refs.list)
+[![Build Status](https://travis-ci.org/opspec-pkgs/opspec.op.list-deps.svg?branch=master)](https://travis-ci.org/opspec-pkgs/opspec.op.list-deps)
 
 # Problem statement
 
-lists ops &/or containers, referenced by (a.k.a. dependencies of) an op
+lists dependencies (containers &/or ops) of an op
 
 # Format
 
@@ -13,25 +13,25 @@ this version of the pkg is in [![opspec 0.1.5](https://img.shields.io/badge/opsp
 ## Install
 
 ```shell
-opctl pkg install github.com/opspec-pkgs/opspec.op.refs.list#1.0.0
+opctl pkg install github.com/opspec-pkgs/opspec.op.list-deps#1.0.0
 ```
 
 ## Run
 
 ```
-opctl run github.com/opspec-pkgs/opspec.op.refs.list#1.0.0
+opctl run github.com/opspec-pkgs/opspec.op.list-deps#1.0.0
 ```
 
 ## Compose
 
 ```yaml
 op:
-  pkg: { ref: github.com/opspec-pkgs/opspec.op.refs.list#1.0.0 }
+  pkg: { ref: github.com/opspec-pkgs/opspec.op.list-deps#1.0.0 }
   inputs:
     # params w/ default
     opDotYml:
   outputs:
-    refs:
+    deps:
 ```
 
 # Support
@@ -39,7 +39,7 @@ op:
 join us on
 [![Slack](https://opspec-slackin.herokuapp.com/badge.svg)](https://opspec-slackin.herokuapp.com/)
 or
-[open an issue](https://github.com/opspec-pkgs/opspec.op.refs.list/issues)
+[open an issue](https://github.com/opspec-pkgs/opspec.op.list-deps/issues)
 
 # Releases
 
--- cmd.js
+++ cmd.js
@@ -2,35 +2,43 @@ const fs = require('fs');
 const path = require('path');
 const jsYaml = require('js-yaml');
 
-const getContainerCallRef = containerCall => {
+const getContainerCallRef = (containerCall, path) => {
   return {
     ref: containerCall.image.ref,
-    type: 'container',
+    path: `${path}.container.image.ref`,
   };
 }
 
-const getOpCallRef = opCall => {
+const getOpCallRef = (opCall, path) => {
   return {
     ref: opCall.pkg.ref,
-    type: 'op',
+    path: `${path}.op.pkg.ref`,
   };
 }
 
-const listCallRefs = call => {
+const listParallelCallRefs = (parallelCall, path) => {
+  return parallelCall.reduce((refs, call, index) => {
+    refs.push(...listCallRefs(call, `${path}.parallel[${index}]`));
+    return refs;
+  }, []);
+}
+
+const listSerialCallRefs = (serialCall, path) => {
+  return serialCall.reduce((refs, call, index) => {
+    refs.push(...listCallRefs(call, `${path}.serial[${index}]`));
+    return refs;
+  }, []);
+}
+
+const listCallRefs = (call, path) => {
   if (call.parallel) {
-    return call.parallel.reduce((refs, call) => {
-      refs.push(...listCallRefs(call));
-      return refs;
-    }, []);
+    return listParallelCallRefs(call.parallel, path);
   } else if (call.serial) {
-    return call.serial.reduce((refs, call) => {
-      refs.push(...listCallRefs(call));
-      return refs;
-    }, []);
+    return listSerialCallRefs(call.serial, path);
   } else if (call.op) {
-    return [getOpCallRef(call.op)];
+    return [getOpCallRef(call.op, path)];
   } else if (call.container) {
-    return [getContainerCallRef(call.container)];
+    return [getContainerCallRef(call.container, path)];
   }
   throw new Error(`unexpected call encountered: ${JSON.stringify(call)}`);
 }
@@ -38,6 +46,6 @@ const listCallRefs = call => {
 const { run } = jsYaml.safeLoad(fs.readFileSync('/op.yml'));
 
 fs.writeFileSync(
-  '/refs',
-  JSON.stringify(listCallRefs(run))
+  '/deps',
+  JSON.stringify(listCallRefs(run, 'run'))
 );
